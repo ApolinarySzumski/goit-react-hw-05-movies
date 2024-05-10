2ef339adcd44eb56edf901e9d9b3a4f8cfbc2f6e
ApolinarySzumski <apolinary@poczta.onet.pl>
2024-05-10 11:35:02 +0200

    first attempt to add routes

---
+++ src/components/MovieDetails.jsx
@@ -0,0 +1,8 @@
+export const MovieDetails = () => {
+  return (
+    <>
+      <button>Go back</button>
+      <div>Information about movie</div>
+    </>
+  );
+};
---
+++ src/components/Reviews.jsx
@@ -0,0 +1,7 @@
+export const Reviews = () => {
+  return (
+    <>
+      <div>Tu wyświetli review</div>
+    </>
+  );
+};
